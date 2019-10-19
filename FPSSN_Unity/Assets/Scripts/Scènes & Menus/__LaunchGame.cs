332c031669e601d1abb06f21e505bb6ea5269161
Corentin VÉROT <verot.corentin@protonmail.com>
2019-10-19 16:05:17 +0200

    Inventaire poursuivit

--- FPSSN_Unity/Assets/Scripts/Scènes & Menus/__LaunchGame.cs
+++ FPSSN_Unity/Assets/Scripts/Scènes & Menus/__LaunchGame.cs
@@ -9,6 +9,10 @@ public class __LaunchGame : MonoBehaviour
     void Start()        // est appelé apres les awake d'initialisation 
     {
         //_MGR_SceneManager.Instance.LoadScene("Scene_Menu");
+
+        // TODO: Bogue : arrêt du temps lors de première partie quand on supprime cette ligne 
+        Time.timeScale = 1f;
+
         _MGR_SceneManager.Instance.LoadScene(0);
     }
 }
