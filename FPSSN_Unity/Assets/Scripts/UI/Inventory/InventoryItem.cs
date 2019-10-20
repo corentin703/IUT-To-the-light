4e10fd3746adc54bf53c7d8637219d288a7a7329
Corentin VÉROT <verot.corentin@protonmail.com>
2019-10-20 15:42:11 +0200

    Oublis de fichiers

---
+++ FPSSN_Unity/Assets/Scripts/UI/Inventory/InventoryItem.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryItem : MonoBehaviour
+{
+    [SerializeField] private Image m_icon;
+    [SerializeField] private Text m_Name;
+    [SerializeField] private Text m_Description;
+    [SerializeField] private Text m_Number;
+
+    //void Awake()
+    //{
+    //    m_icon = GetComponentInChildren<Image>();
+    //    m_Name = GetComponentInChildren<Text>();
+    //    m_Description = GetComponentInChildren<Text>();
+    //    m_Number = GetComponentInChildren<Text>();
+    //}
+
+    public InventoryItem(string sName, string sDescription, int iNumber, Sprite icon)
+    {
+        m_icon.sprite = icon;
+        m_Name.text = sName;
+        m_Description.text = sDescription;
+        m_Number.text = iNumber.ToString();
+    }
+
+    //public void MakeUp(Sprite icon, string sName, string sDescription, int iNumber)
+    //{
+    //    m_icon.sprite = icon;
+    //    m_Name.text = sName;
+    //    m_Description.text = sDescription;
+    //    m_Number.text = iNumber.ToString();
+    //}
+
+
+
+
+}
---
+++ FPSSN_Unity/Assets/Scripts/UI/Inventory/InventoryMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryMenu : MonoBehaviour
+{
+    private List<InventoryItem> m_lInventoryItems;
+
+    public void Actualize()
+    {
+        m_lInventoryItems.Clear();
+
+        foreach (Ressource ressource in _MGR_Ressources.Instance.lRessources)
+            m_lInventoryItems.Add(new InventoryItem(ressource.name, ressource.description, ressource.number, ressource.icon));
+    }
+}
