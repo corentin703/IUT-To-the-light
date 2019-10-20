0fb1367155c518f77866cd366648f5818b5d31f3
Corentin VÉROT <verot.corentin@protonmail.com>
2019-10-20 14:21:55 +0200

    Oublis de fichiers

---
+++ FPSSN_Unity/Assets/Scripts/UI/Events/PauseMenuEvents.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuEvents : MonoBehaviour
+{
+    public void PressBtnResume()
+    {
+        _MGR_UI.Instance.Resume();
+    }
+
+    public void PressBtnBackToMainMenu()
+    {
+        _MGR_UI.Instance.BackToMainMenu();
+    }
+}
---
+++ FPSSN_Unity/Assets/Scripts/UI/InitializeUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InitializeUI : MonoBehaviour
+{
+    [SerializeField]
+    private Text UI_timeLeft;
+
+    [SerializeField]
+    private Text UI_score;
+
+    [SerializeField]
+    private GameObject UI_PausePannel;
+
+    [SerializeField]
+    private GameObject UI_PopUpPannel;
+
+    [SerializeField]
+    private GameObject UI_RessourcesPannel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _MGR_UI.Instance.SetUp(UI_timeLeft, 
+                                UI_score, 
+                                UI_PausePannel, 
+                                UI_PopUpPannel, 
+                                UI_RessourcesPannel
+                            );
+    }
+}
