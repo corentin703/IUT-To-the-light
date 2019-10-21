ec2b82a88326173adbed4cad1a5bdfd715ac7220
DorianVidal <dorianpierrickvidal@gmail.com>
2019-10-21 09:41:29 +0200

    Tentative d'ajout d'un porte

---
+++ FPSSN_Unity/Assets/OuverturePorte.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OuverturePorte : MonoBehaviour
+{
+    Animation myAnim;
+    GameObject Porte;
+    BoxCollider ColliderASupprimer;
+
+    void Start()
+    {
+        myAnim = Porte.GetComponent<Animation>();
+        ColliderASupprimer = Porte.GetComponent<BoxCollider>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if( other.tag == "Player" && Input.GetKeyDown(KeyCode.E))
+        {
+            myAnim.Play();
+            ColliderASupprimer.enabled = !ColliderASupprimer.enabled;
+        }
+    }
+}
