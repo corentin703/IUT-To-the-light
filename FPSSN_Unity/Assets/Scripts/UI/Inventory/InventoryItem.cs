98dcd3302c09423f7ac525f1043010b7a32d68f6
Corentin VÉROT <verot.corentin@protonmail.com>
2019-10-22 15:27:12 +0200

    Scripts inventaires

--- FPSSN_Unity/Assets/Scripts/UI/Inventory/InventoryItem.cs
+++ FPSSN_Unity/Assets/Scripts/UI/Inventory/InventoryItem.cs
@@ -5,36 +5,18 @@ using UnityEngine.UI;
 
 public class InventoryItem : MonoBehaviour
 {
-    [SerializeField] private Image m_icon;
-    [SerializeField] private Text m_Name;
-    [SerializeField] private Text m_Description;
-    [SerializeField] private Text m_Number;
+    [SerializeField] private Image icon;
+    [SerializeField] private Text itemName;
+    [SerializeField] private Text description;
+    [SerializeField] private Text number;
 
-    //void Awake()
-    //{
-    //    m_icon = GetComponentInChildren<Image>();
-    //    m_Name = GetComponentInChildren<Text>();
-    //    m_Description = GetComponentInChildren<Text>();
-    //    m_Number = GetComponentInChildren<Text>();
-    //}
 
     public InventoryItem(string sName, string sDescription, int iNumber, Sprite icon)
     {
-        m_icon.sprite = icon;
-        m_Name.text = sName;
-        m_Description.text = sDescription;
-        m_Number.text = iNumber.ToString();
+        this.icon.sprite = icon;
+        itemName.text = sName;
+        description.text = sDescription;
+        number.text = iNumber.ToString();
     }
-
-    //public void MakeUp(Sprite icon, string sName, string sDescription, int iNumber)
-    //{
-    //    m_icon.sprite = icon;
-    //    m_Name.text = sName;
-    //    m_Description.text = sDescription;
-    //    m_Number.text = iNumber.ToString();
-    //}
-
-
-
 
 }
--- FPSSN_Unity/Assets/Scripts/UI/Inventory/InventoryMenu.cs
+++ FPSSN_Unity/Assets/Scripts/UI/Inventory/InventoryMenu.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InventoryMenu : MonoBehaviour
-{
+{    
     private List<InventoryItem> m_lInventoryItems;
+
+    void Awake()
+    {
+        m_lInventoryItems = new List<InventoryItem>();
+    }
+
 
     public void Actualize()
     {
-        m_lInventoryItems.Clear();
+        //m_lInventoryItems.Clear();
 
-        foreach (Ressource ressource in _MGR_Ressources.Instance.lRessources)
-            m_lInventoryItems.Add(new InventoryItem(ressource.name, ressource.description, ressource.number, ressource.icon));
+        //foreach (Ressource ressource in _MGR_Ressources.Instance.lRessources)
+        //    m_lInventoryItems.Add(new InventoryItem(ressource.name, ressource.description, ressource.number, ressource.icon));
     }
 }
