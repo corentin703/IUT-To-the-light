6bdad91af828dce4242ec5d2c487e92fca1bd434
Corentin VÉROT <verot.corentin@protonmail.com>
2019-10-22 15:05:16 +0200

    Portes annimées et FONCTIONELLES !

---
+++ FPSSN_Unity/Assets/Scripts/GameObjects/Door.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Door : MonoBehaviour
+{
+    [SerializeField] private float pickDistance = 5;
+    /*[SerializeField]*/ private LayerMask layerMask;
+
+    private Animator animator;
+
+    // TODO: Mettre le système d'objet(s) nécéssaire(s) (genre des clés) au point quand l'inventaire sera finalisé
+    [HideInInspector] // À supprimer lors de cette mise en place afin de permettre l'édition
+    public List<Ressource> gameObjectsNeeded = new List<Ressource>();
+
+    [HideInInspector]
+    public bool isOpened = false;
+
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        isOpened = animator.GetBool("isOpened");
+
+        layerMask = LayerMask.GetMask("Toggable");
+    }
+
+    void Update()
+    {
+        if (gameObjectsNeeded.Count == 0)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                RaycastHit hitInfo = new RaycastHit();
+
+                if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo, pickDistance, layerMask))
+                {
+                    if (hitInfo.transform.gameObject == gameObject)
+                    {
+                        Debug.Log("Opening a door");
+                        isOpened = !isOpened;
+                        animator.SetBool("isOpened", isOpened);
+                    }
+                }
+            }
+        }
+        else
+        {
+            foreach (Ressource ressource in gameObjectsNeeded)
+            {
+                if (_MGR_Ressources.Instance.lRessources.Contains(ressource))
+                    gameObjectsNeeded.Remove(ressource);
+            }
+        }
+    }
+}
