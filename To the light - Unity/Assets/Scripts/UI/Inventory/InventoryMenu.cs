708391fc2f5865c164a71a7a69e6b47ef68c7e6f
Corentin VÉROT <verot.corentin@protonmail.com>
2019-10-30 16:59:07 +0100

    Barre de scrolling OK

--- To the light - Unity/Assets/Scripts/UI/Inventory/InventoryMenu.cs
+++ To the light - Unity/Assets/Scripts/UI/Inventory/InventoryMenu.cs
@@ -1,24 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class InventoryMenu : MonoBehaviour
+public class InventoryMenu : MonoBehaviour, IScrollHandler
 {
     private Dictionary<Ressource, InventoryItem> m_dInventoryItem = new Dictionary<Ressource, InventoryItem>();
     private Scrollbar m_scrollbar;
-    
+
+    private RectTransform TransRef;
+    private ScrollRect ScrollRef;
+    private RectTransform ContentRef;
+
+    private float MinScroll = 0;
+    private float MaxScroll;
+
     [SerializeField]
     private GameObject templateItem;
 
     [SerializeField]
     private GameObject viewport;
 
-    void Awake()
+    [SerializeField]
+    private float ScrollSpeed = 100;
+
+
+    void Start()
     {
         m_scrollbar = GetComponentInChildren<Scrollbar>();
+        TransRef = GetComponent<RectTransform>();
+        ScrollRef = GetComponentInChildren<ScrollRect>();
+        ContentRef = ScrollRef.content;
     }
-
 
     public void Actualize()
     {
@@ -33,11 +47,26 @@ public class InventoryMenu : MonoBehaviour
             m_dInventoryItem[ressource].SetItemInfos(ressource.GetName(), ressource.GetDescription(), ressource.GetNumber());
         }
 
+        MaxScroll = ContentRef.rect.height - TransRef.rect.height;
+
         m_scrollbar.numberOfSteps = m_dInventoryItem.Count;
+        //m_scrollbar.size = 1 / m_dInventoryItem.Count;
     }
 
     public void Close()
     {
         _MGR_UI.Instance.CloseRessources();
+    }
+
+    public void OnScroll(PointerEventData eventData)
+    {
+        Vector2 ScrollDelta = eventData.scrollDelta;
+
+        ContentRef.anchoredPosition += new Vector2(0, -ScrollDelta.y * ScrollSpeed);
+
+        if (ContentRef.anchoredPosition.y < MinScroll)
+            ContentRef.anchoredPosition = new Vector2(0, MinScroll);
+        else if (ContentRef.anchoredPosition.y > MaxScroll)
+            ContentRef.anchoredPosition = new Vector2(0, MaxScroll);
     }
 }
