1d578d0760740dfc0a07d208fdcef3f012e162d0
Corentin VÉROT <verot.corentin@protonmail.com>
2019-11-06 20:43:26 +0100

    Oublis d'objets

---
+++ To the light - Unity/Assets/Scripts/GameObjects/Coin.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : Ressource
+{
+    private static uint m_pickedNumber = 0;
+    public override uint PickedNumber
+    {
+        get { return m_pickedNumber; }
+        protected set { m_pickedNumber = value; }
+    }
+    public override void Add(uint num = 1)
+    {
+        PickedNumber += num;
+    }
+}
